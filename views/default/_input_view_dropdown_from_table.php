1dda6f03b5c92e2e988ac6e3e723d5be0dffbf2d
Mark Wadham <mark.wadham@openeyes.org.uk>
2013-02-01 11:17:51 +0000

    [OE-2589] don't bother showing input fields in the report summary for dropdowns left on 'all'

--- views/default/_input_view_dropdown_from_table.php
+++ views/default/_input_view_dropdown_from_table.php
@@ -1,4 +1,6 @@
-<tr>
-	<th><?php echo $input->description?>:</th>
-	<td><?php echo @$_POST[$input->name] ? $input['data_type_param1']::model()->findByPk(@$_POST[$input->name])->reportDisplay : 'All'?></td>
-</tr>
+<?php if (@$_POST[$input->name]) {?>
+	<tr>
+		<th><?php echo $input->description?>:</th>
+		<td><?php echo @$_POST[$input->name] ? $input['data_type_param1']::model()->findByPk(@$_POST[$input->name])->reportDisplay : 'All'?></td>
+	</tr>
+<?php }?>
