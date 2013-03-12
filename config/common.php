ee45c8b46fab2b82d19724ff42ea770201f40e01
Mark <m@rkw.me>
2013-03-12 11:27:18 +0000

    config for Reports menu item

---
+++ config/common.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+	'params' => array(
+		'menu_bar_items' => array(
+			'reports' => array(
+				'title' => 'Reports',
+				'uri' => 'Reports/default/index',
+				'position' => 40,
+			),
+		),
+	)
+);
